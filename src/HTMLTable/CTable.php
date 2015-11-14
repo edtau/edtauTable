f4f2f402cd1c2b2e11094f9d8652aa193d7ec6c5
edtau <eddie.taube@gmail.com>
2015-11-13 22:33:06 -0600

    Added the files

---
+++ src/HTMLTable/CTable.php
@@ -0,0 +1,148 @@
+<?php
+namespace Edtau\HTMLTable;
+
+use Exception;
+use InvalidArgumentException;
+
+class CTable
+{
+
+    private $tableHeader = null;
+    private $rows = null;
+    private $displayExceptions = false;
+    private $exception = false;
+    public function __construct($displayExceptions = false)
+    {
+        $this->displayExceptions = $displayExceptions;
+    }
+    public function getExecption(){
+
+        return $this->exception;
+
+    }
+    public function getHeader()
+    {
+        return $this->tableHeader;
+    }
+
+    public function getRows()
+    {
+        return $this->rows;
+    }
+
+    public function validateArray($array)
+    {
+        try{
+             if (is_array($array) == false)
+                throw new Exception("is not a valid array: ".$array);
+        } catch (Exception $e) {
+            if($this->displayExceptions){
+                echo  $e->getMessage();
+            }
+            $this->exception =  $e->getMessage();
+            return false;
+        }
+        return true;
+
+    }
+
+    /**
+     * @param $array the values used to get a table row
+     * @return string the table row
+     */
+    public function addRow($array)
+    {
+        if ($this->validateArray($array)) {
+            $td = "<tr>";
+            foreach ($array as $value) {
+                $td .= "<td>$value</td>";
+            }
+            $td .= "</tr>";
+            $this->rows[] = $td;
+            return $td;
+        }
+        return false;
+    }
+
+    /**
+     * Function to generate the tablehead
+     * @param $theadData the data for the tablehead
+     * @return string the tablehead
+     */
+    public function addHeader($array)
+    {
+        if ($this->validateArray($array)) {
+            $this->tableHeader = "<thead>\n<tr>\n";
+            foreach ($array as $head) {
+                $this->tableHeader .= "<th>$head</th>";
+            }
+            $this->tableHeader .= "</tr>\n</thead>\n";
+            return $this->tableHeader;
+        }
+        return false;
+    }
+
+    public function getTable($id = null)
+    {
+
+        try {
+            if ($this->tableHeader != null AND $this->rows != null) {
+                $table = "<table>";
+                $table .= $this->tableHeader;
+                foreach ($this->rows as $row) {
+                    $table .= $row;
+                }
+                $table .= "</table>";
+                return $table;
+            }
+            throw new InvalidArgumentException("You have not added the table header and rows");
+        } catch (Exception $e) {
+            $this->exception = $e->getMessage();
+        }
+        return false;
+
+
+    }
+
+    /**
+     * Function to get the generated table
+     * the first row in the array becomes the head of
+     * the table if the tableheader is not set
+     * @param $array the table data
+     * @param null $id optional to use id for table
+     * @return string the complete table
+     */
+    public function simpleTable($array, $id = null)
+    {
+
+        if($this->validateArray($array) == false){
+            return false;
+        }
+        $table = "<table>\n";
+
+        if ($id != null) {
+            $table = "<table id='$id'>\n";
+        }
+
+        if ($this->tableHeader != null) {
+            $thead = $this->tableHeader;
+         } else {
+            $theadData = array_shift($array);
+            $thead = $this->addHeader($theadData);
+        }
+        $table .= $thead;
+        $row = "";
+
+        foreach ($array as $value) {
+            $row .= $this->addRow($value);
+        }
+
+        $table .= $row;
+        $table .= "</table>\n";
+        return $table;
+    }
+
+
+
+
+}
---
+++ test/HTMLTable/CTableTest.php
@@ -0,0 +1,107 @@
+<?php
+
+//namespace Edtau\HTMLTable;
+use Edtau\HTMLTable\CTable as CTable;
+
+class CTableTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testAddHeader()
+    {
+        $table = new CTable(false);
+        $header = $table->addHeader(array("data"));
+        $this->assertContains("<thead>", $header);
+        $this->assertContains("</thead>", $header);
+        $this->assertContains("<tr>", $header);
+        $this->assertContains("</tr>", $header);
+        $this->assertContains("<th>", $header);
+        $this->assertContains("</th>", $header);
+        $this->assertContains("data", $header);
+
+        $this->assertFalse($table->addHeader("string"));
+    }
+
+    public function testTableRow()
+    {
+        $table = new CTable(false);
+        $row = $table->addRow(array("data"));
+        $this->assertContains("<tr>", $row);
+        $this->assertContains("<td>", $row);
+        $this->assertContains("</td>", $row);
+        $this->assertContains("</tr>", $row);
+        $this->assertContains("data", $row);
+        $row = $table->addRow("string instead of array");
+        $this->assertFalse($row);
+        $table = new CTable(false);
+        $this->assertNull($table->getRows());
+
+        $table->addRow(array("frank", "anna"));
+        $table->addRow(array("frank", "anna"));
+        $this->assertCount(2, $table->getRows());
+
+
+    }
+
+    public function testValidateArray()
+    {
+        $table = new CTable(false);
+        $result = $table->validateArray("no-array");
+        $this->assertFalse($result);
+        $result = $table->validateArray(array("hello", "hello"));
+        $this->assertTrue($result);
+    }
+
+    public function testGetTable()
+    {
+        $table = new CTable(false);
+        $result = $table->getTable();
+        $this->assertFalse($result);
+        $table->addHeader(array("name", "age"));
+        $table->addRow(array("frank", "anna"));
+
+        $result = $table->getTable();
+
+        $this->assertStringStartsWith('<table>', $result);
+        $this->assertStringEndsWith('</table>', $result);
+    }
+
+    public function testGetHeader()
+    {
+        $table = new CTable();
+        $this->assertNull($table->getHeader());
+    }
+
+    public function testSimpleTable()
+    {
+        $array = array(
+            array("name", "age"),
+            array("sven", "13"),
+            array("lars", "14")
+        );
+        $table = new CTable();
+        $result = $table->simpleTable("string");
+        $this->assertFalse($result);
+
+        $booleanResult = false;
+        if (is_string($table->simpleTable($array))) {
+            $booleanResult = true;
+        }
+        $this->assertTrue($booleanResult);
+    }
+
+    public function testGetException()
+    {
+        $table = new CTable();
+
+        $this->assertFalse($table->getExecption());
+        $table = new CTable(true);
+        $this->assertFalse($table->getExecption());
+        $result = $table->validateArray("string");
+        var_dump($result);
+        if($result != false){
+            $this->assertFalse($table->getExecption());
+        }
+
+    }
+}
